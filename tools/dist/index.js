5b549c05d6318a4f3016815fe4deaa26fa5d9a28
kristen <kristen.schulte@huskers.unl.edu>
2022-06-22 13:33:22 -0500

    Ch 5 to pg 127.

--- tools/dist/index.js
+++ tools/dist/index.js
@@ -1,5 +1,13 @@
-var printMessage = function (msg) {
-    console.log("Message: " + msg);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calc_1 = require("./calc");
+let printMessage = (msg) => {
+    console.log(`Message: ${msg}`);
 };
 printMessage("Hello, TypeScript");
-printMessage("It is sunny today");
+let total = calc_1.sum(100, 200, 300);
+console.log(`Total: ${total}`);
+// let data = new Map();
+// data.set("Bob", "London");
+// data.set("Alice", "Paris");
+// data.forEach((val, key) => console.log(`${key} lives in ${val}`));
--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,6 +1,15 @@
+import { sum } from "./calc";
+
 let printMessage = (msg: string): void => {
     console.log(`Message: ${msg}`);
 }
 
 printMessage("Hello, TypeScript");
-printMessage("It is sunny today");
+
+let total = sum(100, 200, 300);
+console.log(`Total: ${total}`);
+
+// let data = new Map();
+// data.set("Bob", "London");
+// data.set("Alice", "Paris");
+// data.forEach((val, key) => console.log(`${key} lives in ${val}`));
