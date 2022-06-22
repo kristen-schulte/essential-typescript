6d2ca776f439cdad3cf4c17a3445cbc2f6151eae
kristen <kristen.schulte@huskers.unl.edu>
2022-06-22 05:22:12 -0500

    Ch4 to pg 95.

--- primer/index.js
+++ primer/index.js
@@ -1,28 +1,75 @@
-let Product = function (name, price) {
-  this.name = name;
-  this.price = price;
-};
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 
-Product.prototype.toString = function () {
-  return `toString: Name: ${this.name}, Price: ${this.price}`;
-};
+  toString() {
+    return `toString: Name: ${this.name}, Price: ${this.price}`;
+  }
+}
 
-Product.process = (...products) =>
-  products.forEach((p) => console.log(p.toString()));
+class GiftPack {
+  constructor(name, prod1, prod2, prod3) {
+    this.name = name;
+    this.prod1 = prod1;
+    this.prod2 = prod2;
+    this.prod3 = prod3;
+  }
 
-Product.process(new Product("Hat", 100, 1.2), new Product("Boots", 100));
+  getTotalPrice() {
+    return [this.prod1, this.prod2, this.prod3].reduce(
+      (total, p) => total + p.price,
+      0
+    );
+  }
 
-// let TaxedProduct = function (name, price, taxRate) {
-//   Product.call(this, name, price);
-//   this.taxRate = taxRate;
-// };
-// Object.setPrototypeOf(TaxedProduct.prototype, Product.prototype);
-// TaxedProduct.prototype.getPriceIncTax = function () {
-//   return Number(this.price) * this.taxRate;
-// };
-// TaxedProduct.prototype.toTaxString = function () {
-//   return `${this.toString()}, Tax: ${this.getPriceIncTax()}`;
-// };
+  *[Symbol.iterator]() {
+    yield this.prod1;
+    yield this.prod2;
+    yield this.prod3;
+  }
+}
 
-// let hat = new TaxedProduct("Hat", 100, 1.2);
-// let boots = new Product("Boots", 100);
+let winter = new GiftPack(
+  "winter",
+  new Product("Hat", 100),
+  new Product("Boots", 80),
+  new Product("Gloves", 23)
+);
+
+console.log(`Total price: ${winter.getTotalPrice()}`);
+[...winter].forEach((p) => console.log(`Product: ${p}`));
+
+// function* createProductIterator() {
+//   yield new Product("Hat", 100);
+//   yield new Product("Boots", 100);
+//   yield new Product("Umbrella", 23);
+// }
+
+// [...createProductIterator()].forEach((p) => console.log(p.toString()));
+
+// class TaxedProduct extends Product {
+//   constructor(name, price, taxRate = 1.2) {
+//     super(name, price);
+//     this.taxRate = taxRate;
+//   }
+
+//   getPriceIncTax() {
+//     return Number(this.price) * this.taxRate;
+//   }
+
+//   toString() {
+//     let chainResult = super.toString();
+//     return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
+//   }
+
+//   static process(...products) {
+//     products.forEach((p) => console.log(p.toString()));
+//   }
+// }
+
+// TaxedProduct.process(
+//   new TaxedProduct("Hat", 100, 1.2),
+//   new TaxedProduct("Boots", 100)
+// );
