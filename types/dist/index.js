a29f07d4c5e18ca5672acb73059136b6911726c6
kristen <kristen.schulte@huskers.unl.edu>
2022-06-24 07:44:01 -0500

    Ch 9 to pg 214.

--- types/dist/index.js
+++ types/dist/index.js
@@ -4,23 +4,23 @@ function calculateTax(amount) {
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-let hat = ["Hat", 100, 10, 1.2, 3, .95];
-let gloves = ["Gloves", 75, 10];
-[hat, gloves].forEach(tuple => {
-    let [name, price, taxRate, ...coupons] = tuple;
-    if (taxRate != undefined) {
-        price += price * (taxRate / 100);
+var Product;
+(function (Product) {
+    Product[Product["Hat"] = 0] = "Hat";
+    Product[Product["Gloves"] = 1] = "Gloves";
+    Product[Product["Umbrella"] = 2] = "Umbrella";
+})(Product || (Product = {}));
+let products = [[Product.Hat, 100], [Product.Gloves, 75]];
+products.forEach((prod) => {
+    switch (prod[0]) {
+        case Product.Hat:
+            writePrice("Hat", calculateTax(prod[1]));
+            break;
+        case Product.Gloves:
+            writePrice("Gloves", calculateTax(prod[1]));
+            break;
+        case Product.Umbrella:
+            writePrice("Umbrella", calculateTax(prod[1]));
+            break;
     }
-    coupons.forEach(c => price -= c);
-    writePrice(name, price);
 });
-// let products: [string, number][] = [["Hat", 100], ["Gloves", 75]];
-// let tupleUnion: ([string, number] | boolean)[] = [true, false, hat, ...products];
-// tupleUnion.forEach((elem: [string, number] | boolean) => {
-//     if (elem instanceof Array) {
-//         let [str, num] = elem;
-//         writePrice(str, calculateTax(num));
-//     } else if (typeof elem === "boolean") {
-//         console.log(`Boolean Value: ${elem}`);
-//     }
-// });
--- types/src/index.ts
+++ types/src/index.ts
@@ -6,27 +6,20 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let hat: [string, number, number?, ...number[]] = ["Hat", 100, 10, 1.2, 3, .95];
-let gloves: [string, number, number?, ...number[]] = ["Gloves", 75, 10];
+enum Product { Hat, Gloves, Umbrella }
 
-[hat, gloves].forEach(tuple => {
-    let [name, price, taxRate, ...coupons] = tuple;
+let products: [Product, number][] = [[Product.Hat, 100], [Product.Gloves, 75]];
 
-    if (taxRate != undefined) {
-        price += price * (taxRate / 100);
+products.forEach((prod: [Product, number]) => {
+    switch (prod[0]) {
+        case Product.Hat:
+            writePrice("Hat", calculateTax(prod[1]));
+            break;
+        case Product.Gloves:
+            writePrice("Gloves", calculateTax(prod[1]));
+            break;
+        case Product.Umbrella:
+            writePrice("Umbrella", calculateTax(prod[1]));
+            break;
     }
-    coupons.forEach(c => price -= c);
-    writePrice(name, price);
 })
-
-// let products: [string, number][] = [["Hat", 100], ["Gloves", 75]];
-// let tupleUnion: ([string, number] | boolean)[] = [true, false, hat, ...products];
-
-// tupleUnion.forEach((elem: [string, number] | boolean) => {
-//     if (elem instanceof Array) {
-//         let [str, num] = elem;
-//         writePrice(str, calculateTax(num));
-//     } else if (typeof elem === "boolean") {
-//         console.log(`Boolean Value: ${elem}`);
-//     }
-// });
