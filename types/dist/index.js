a897dad800797c2c9a04db629309d6eb357dad8a
kristen <kristen.schulte@huskers.unl.edu>
2022-06-25 07:00:52 -0500

    Ch 12 to pg 294.

--- types/dist/index.js
+++ types/dist/index.js
@@ -5,7 +5,8 @@ let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100),
     new dataTypes_1.Product("Hat", 25)];
-class PeopleCollection {
+// type dataType = Person | Product;
+class DataCollection {
     constructor(initialItems) {
         this.items = [];
         this.items.push(...initialItems);
@@ -13,14 +14,19 @@ class PeopleCollection {
     add(newItem) {
         this.items.push(newItem);
     }
-    getNames() {
-        return this.items.map(item => item.name);
-    }
+    // getNames(): string[] {
+    //     return this.items.map(item => item.name);
+    // }
     getItem(index) {
         return this.items[index];
     }
 }
-let peopleData = new PeopleCollection(people);
-console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let peopleData = new DataCollection(people);
+// console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
+// if (firstPerson instanceof Person) {
 console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+//}
+let productData = new DataCollection(products);
+let firstProduct = productData.getItem(0);
+console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
--- types/src/index.ts
+++ types/src/index.ts
@@ -6,29 +6,37 @@ new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100),
 new Product("Hat", 25)];
 
-class PeopleCollection {
-    private items: Person[] = [];
+// type dataType = Person | Product;
 
-    constructor(initialItems: Person[]) {
+class DataCollection<T> {
+    private items: T[] = [];
+
+    constructor(initialItems: T[]) {
         this.items.push(...initialItems);
     }
 
-    add(newItem: Person) {
+    add(newItem: T) {
         this.items.push(newItem);
     }
 
-    getNames(): string[] {
-        return this.items.map(item => item.name);
-    }
+    // getNames(): string[] {
+    //     return this.items.map(item => item.name);
+    // }
 
-    getItem(index: number): Person {
+    getItem(index: number): T {
         return this.items[index];
     }
 }
 
-let peopleData = new PeopleCollection(people);
+let peopleData = new DataCollection<Person>(people);
 
-console.log(`Names: ${peopleData.getNames().join(", ")}`);
+// console.log(`Names: ${peopleData.getNames().join(", ")}`);
 
 let firstPerson = peopleData.getItem(0);
+// if (firstPerson instanceof Person) {
 console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+//}
+
+let productData = new DataCollection<Product>(products);
+let firstProduct = productData.getItem(0);
+console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
