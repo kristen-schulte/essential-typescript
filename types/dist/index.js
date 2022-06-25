8d9280ea90d010fc8669a7669b12da67ed06ebc5
kristen <kristen.schulte@huskers.unl.edu>
2022-06-25 06:53:49 -0500

    Ch 12 to pg 290.

--- types/dist/index.js
+++ types/dist/index.js
@@ -5,4 +5,22 @@ let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100),
     new dataTypes_1.Product("Hat", 25)];
-[...people, ...products].forEach(item => console.log(`Item: ${item.name}`));
+class PeopleCollection {
+    constructor(initialItems) {
+        this.items = [];
+        this.items.push(...initialItems);
+    }
+    add(newItem) {
+        this.items.push(newItem);
+    }
+    getNames() {
+        return this.items.map(item => item.name);
+    }
+    getItem(index) {
+        return this.items[index];
+    }
+}
+let peopleData = new PeopleCollection(people);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
