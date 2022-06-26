dcaf59c419aa8d9fbb48b440f01b71d6ff695eda
kristen <kristen.schulte@huskers.unl.edu>
2022-06-26 17:20:03 -0500

    Ch 14 to pg 357.

---
+++ usingjs/src/formatters.js
@@ -0,0 +1,13 @@
+//@ts-nocheck
+
+export function sizeFormatter(thing, count) {
+  writeMessage(`The ${thing} has ${count} items`);
+}
+
+export function costFormatter(thing, cost) {
+  writeMessage(`The ${thing} costs $${cost.toFixed(2)}`, true);
+}
+
+function writeMessage(message) {
+  console.log(message);
+}
--- usingjs/src/index.ts
+++ usingjs/src/index.ts
@@ -1,5 +1,6 @@
 import { SportsProduct, SPORT } from "./product";
 import { Cart } from "./cart";
+import { sizeFormatter, costFormatter } from "./formatters";
 
 const kayak = new SportsProduct(1, "Kayak", 275, SPORT.Watersports);
 const hat = new SportsProduct(2, "Hat", 22.10, SPORT.Running, SPORT.Watersports);
@@ -10,5 +11,5 @@ cart.addProduct(kayak, 1);
 cart.addProduct(hat, 1);
 cart.addProduct(hat, 2);
 
-console.log(`Cart has ${cart.itemCount} items`);
-console.log(`Cart value is $${cart.totalPrice.toFixed(2)}`);
+sizeFormatter("Cart", cart.itemCount);
+costFormatter("Cart", cart.totalPrice);
