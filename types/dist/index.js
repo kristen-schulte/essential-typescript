1236177cba7136048cb14c73667315f028ac0755
kristen <kristen.schulte@huskers.unl.edu>
2022-06-26 14:39:04 -0500

    Ch 13 to pg 343.

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-function FilterArray(data, predicate) {
-    return data.filter(item => !predicate(item));
+function convertProduct(p) {
+    return { name: p.name, price: `$${p.price.toFixed(2)}` };
 }
-const dataArray = [new dataTypes_1.Product("Kayak", 275), new dataTypes_1.Person("Bob", "London"), new dataTypes_1.Product("Lifejacket", 27.5)];
-function isProduct(item) {
-    return item instanceof dataTypes_1.Product;
-}
-const filteredData = FilterArray(dataArray, isProduct);
-filteredData.forEach(item => console.log(`Person: ${item.name}`));
+const kayak = convertProduct(new dataTypes_1.Product("Kayak", 275));
+console.log(`Product: ${kayak.name}, ${kayak.price}`);
