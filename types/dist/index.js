3b5fca30ddab3f2452243afe7aaaad468e1049b9
kristen <kristen.schulte@huskers.unl.edu>
2022-06-26 10:22:39 -0500

    Ch 13 to pg 331.

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,40 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const dataTypes_1 = require("./dataTypes");
-function getValue(item, keyname) {
-    return item[keyname];
-}
-// type priceType = Product["price"];
-// type allTypes = Product[keyof Product];
-// let p = new Product("Running Shoes", 100);
-// console.log(getValue<Product, "name">(p, "name"));
-// console.log(getValue(p, "price"));
-// let e = new Employee("Bob Smith", "Sales");
-// console.log(getValue(e, "name"));
-// console.log(getValue(e, "role"));
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-// type shapeType = { name: string };
-class Collection {
-    constructor(initialItems = [], propertyName) {
-        this.propertyName = propertyName;
-        this.items = new Map();
-        this.add(...initialItems);
-    }
-    add(...newItems) {
-        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
-    }
-    get(key) {
-        return this.items.get(key);
-    }
-    get count() {
-        return this.items.size;
-    }
-    [Symbol.iterator]() {
-        return this.items.values();
-    }
-}
-const productCollection = new Collection(products, "price");
-console.log(`There are ${productCollection.count} products`);
-let itemByKey = productCollection.get(100);
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
-// [...productCollection].forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
+let p = { name: "Kayak", price: 275 };
+console.log(`Mapped type: ${p.name}, ${p.price}`);
+let q = { name: "Kayak", price: "apples" };
+console.log(`Changed type # 1: ${q.name}, ${q.price}`);
+let r = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type # 2: ${r.nameProperty}, ${r.priceProperty}`);
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,52 +1,22 @@
 import { City, Person, Product, Employee } from "./dataTypes";
 
-function getValue<T, K extends keyof T>(item: T, keyname: K): T[K] {
-    return item[keyname];
+type MappedProduct = {
+    [P in keyof Product]: Product[P]
+};
+
+let p: MappedProduct = { name: "Kayak", price: 275 };
+console.log(`Mapped type: ${p.name}, ${p.price}`);
+
+type AllowStrings = {
+    [P in keyof Product]: Product[P] | string
 }
 
-// type priceType = Product["price"];
-// type allTypes = Product[keyof Product];
+let q: AllowStrings = { name: "Kayak", price: "apples" };
+console.log(`Changed type # 1: ${q.name}, ${q.price}`);
 
-// let p = new Product("Running Shoes", 100);
-// console.log(getValue<Product, "name">(p, "name"));
-// console.log(getValue(p, "price"));
-
-// let e = new Employee("Bob Smith", "Sales");
-// console.log(getValue(e, "name"));
-// console.log(getValue(e, "role"));
-
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-
-// type shapeType = { name: string };
-
-class Collection<T, K extends keyof T> implements Iterable<T>{
-    private items: Map<T[K], T>;
-    constructor(initialItems: T[] = [], private propertyName: K) {
-        this.items = new Map<T[K], T>();
-        this.add(...initialItems);
-    }
-
-    add(...newItems: T[]): void {
-        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
-    }
-
-    get(key: T[K]): T {
-        return this.items.get(key);
-    }
-
-    get count(): number {
-        return this.items.size;
-    }
-
-    [Symbol.iterator](): Iterator<T> {
-        return this.items.values();
-    }
+type ChangeNames = {
+    [P in keyof Product as `${P}Property`]: Product[P]
 }
 
-const productCollection = new Collection(products, "price");
-console.log(`There are ${productCollection.count} products`);
-
-let itemByKey = productCollection.get(100);
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
-
-// [...productCollection].forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
+let r: ChangeNames = { nameProperty: "Kayak", priceProperty: 12 };
+console.log(`Changed type # 2: ${r.nameProperty}, ${r.priceProperty}`);
