da8081d11142a7a7f5ec95375db10830943935a1
kristen <kristen.schulte@huskers.unl.edu>
2022-07-08 15:36:13 -0500

    Still doesn't display.

--- webapp/src/htmlDisplay.tsx
+++ webapp/src/htmlDisplay.tsx
@@ -1,24 +1,47 @@
 import { createElement } from "./tools/jsxFactory";
 import { Product, Order } from "./data/entities";
+import { AbstractDataSource } from "./data/abstractDataSource";
+import { ProductList } from "./productList";
 
 export class HtmlDisplay {
-  props: {
-    products: Product[];
-    order: Order;
-  };
+  private containerElem: HTMLElement;
+  private selectedCategory: string;
 
-  getContent(): HTMLElement {
-    return (
-      <h3 className="bg-secondary text-center text-white p-2">
-        {this.getElementText()}
-      </h3>
-    );
+  constructor () {
+    this.containerElem = document.createElement("div");
   }
 
-  getElementText() {
-    return (
-      `${this.props.products.length} Products, ` +
-      `Order total: $${this.props.order.total}`
-    );
+  props: {
+    dataSource: AbstractDataSource;
+  };
+
+  async getContent(): Promise<HTMLElement> {
+    await this.updateContent();
+    return this.containerElem;
+  }
+
+  async updateContent() {
+    const products = await this.props.dataSource.getProducts("id", this.selectedCategory);
+    const categories = await this.props.dataSource.getCategories();
+    const content = <div>
+      <ProductList 
+        products={products}
+        categories={categories}
+        selectedCategory={this.selectedCategory}
+        addToOrderCallback={this.addToOrder}
+        filterCallback={this.selectCategory} />
+    </div>
+
+    this.containerElem.appendChild(content);
+  }
+
+  addToOrder = (product: Product, quantity: number) => {
+    this.props.dataSource.order.addProduct(product, quantity);
+    this.updateContent();
+  }
+
+  selectCategory = (selected: string) => {
+    this.selectedCategory = selected === "All" ? undefined : selected;
+    this.updateContent();
   }
 }
