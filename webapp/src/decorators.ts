b3ad34147dfb2a773390a92649a0236db02c28ee
kristen <kristen.schulte@huskers.unl.edu>
2022-07-10 07:53:50 -0500

    Use decorator to set minimum.

---
+++ webapp/src/decorators.ts
@@ -0,0 +1,11 @@
+export const minimumValue = (propName: string, min: number) =>
+    (constructor: any, methodName: string, descriptor: PropertyDescriptor): any => {
+        const origFunction = descriptor.value;
+        descriptor.value = async function wrapper(...args) {
+            const results = await origFunction.apply(this, args);
+            return results.map(r => ({
+                ...r, [propName]: r[propName] < min
+                    ? min : r[propName]
+            }));
+        }
+    }
