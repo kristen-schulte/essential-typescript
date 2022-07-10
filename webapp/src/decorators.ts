6c231d297cef45b6890dcb612844cea6702da53f
kristen <kristen.schulte@huskers.unl.edu>
2022-07-10 08:08:51 -0500

    Using decorator metadata.

--- webapp/src/decorators.ts
+++ webapp/src/decorators.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata";
+
 export const minimumValue = (propName: string, min: number) =>
     (constructor: any, methodName: string, descriptor: PropertyDescriptor): any => {
         const origFunction = descriptor.value;
@@ -7,5 +9,19 @@ export const minimumValue = (propName: string, min: number) =>
                 ...r, [propName]: r[propName] < min
                     ? min : r[propName]
             }));
+        }
+    }
+
+export const addClass = (selector: string, ...classNames: string[]) =>
+    (constructor: any, methodName: string, descriptor: PropertyDescriptor): any => {
+        if (Reflect.getMetadata("design:returntype",
+            constructor, methodName) === HTMLElement) {
+            const origFunction = descriptor.value;
+            descriptor.value = function wrapper(...args) {
+                let content: HTMLElement = origFunction.apply(this, args);
+                content.querySelectorAll(selector).forEach(elem =>
+                    classNames.forEach(c => elem.classList.add(c)));
+                return content;
+            }
         }
     }
