5175d0f0d7f7e8bd945dd25fba48f954e87d8f3e
kristen <kristen.schulte@huskers.unl.edu>
2022-07-10 17:59:38 -0500

    Build for production.

--- angularapp/src/app/data/remoteDataSource.ts
+++ angularapp/src/app/data/remoteDataSource.ts
@@ -5,14 +5,10 @@ import { map } from "rxjs/operators";
 import { DataSourceImpl } from "./dataSource";
 import { Product, Order } from "./entities";
 
-const protocol = "http";
-const hostname = "localhost";
-const port = "4600";
-
 const urls = {
-    products: `${protocol}://${hostname}:${port}/products`,
-    orders: `${protocol}://${hostname}:${port}/orders`
-}
+    products: "/api/products",
+    orders: "/api/orders"
+};
 
 @Injectable()
 export class RemoteDataSource extends DataSourceImpl {
